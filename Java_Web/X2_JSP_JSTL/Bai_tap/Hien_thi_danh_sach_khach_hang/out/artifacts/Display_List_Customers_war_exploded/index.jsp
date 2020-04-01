0b4f757543f0531be705c81c3a1c3277040f01c4
LeDinhQuoc24 <kudo2412@gmail.com>
2020-04-01 10:49:51 +0700

    addfile

---
+++ Java_Web/X2_JSP_JSTL/Bai_tap/Hien_thi_danh_sach_khach_hang/out/artifacts/Display_List_Customers_war_exploded/index.jsp
@@ -0,0 +1,29 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Admin
+  Date: 4/1/2020
+  Time: 9:07 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <table cellpadding="30px" cellspacing="0">
+    <h2>Danh sách khách hàng</h2>
+    <tr>
+      <th>Tên</th>
+      <th>Ngày sinh</th>
+      <th>Địa chỉ</th>
+      <th>Ảnh</th>
+    </tr>
+
+
+
+  </table>
+  </body>
+</html>
---
+++ Java_Web/X2_JSP_JSTL/Bai_tap/Hien_thi_danh_sach_khach_hang/src/Customer.java
@@ -0,0 +1,52 @@
+import java.text.DateFormat;
+
+public class Customer {
+    private String name;
+    private String hireDate;
+    private String address;
+    private String image;
+
+
+    public Customer() {
+    }
+
+    public Customer(String name, String address, String image, String hireDate) {
+        this.name = name;
+        this.address = address;
+        this.image = image;
+        this.hireDate = hireDate;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
