0094a41484fe875a810db7e94138b3e818a7174d
LeDinhQuoc24 <kudo2412@gmail.com>
2020-04-06 14:38:33 +0700

    addfile

--- Java_Web/X4_Tong_quan_Spring_MVC/Thuc_hanh/src/controllers/GreetingController.java
+++ Java_Web/X4_Tong_quan_Spring_MVC/Thuc_hanh/src/controllers/GreetingController.java
@@ -1,12 +1,18 @@
 package controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GreetingController {
+//    @GetMapping("/greeting")
+//    public String greeting(){
+//        return "index1";
+//    }
     @GetMapping("/greeting")
-    public String greeting(){
+    public String getIndex(Model model){
+        model.addAttribute("hello","Greeting hello class");
         return "index1";
     }
 }
--- Java_Web/X4_Tong_quan_Spring_MVC/Thuc_hanh/web/WEB-INF/views/index1.jsp
+++ Java_Web/X4_Tong_quan_Spring_MVC/Thuc_hanh/web/WEB-INF/views/index1.jsp
@@ -11,6 +11,7 @@
     <title>Greeting</title>
 </head>
 <body>
-<h1>Hello Spring MVC</h1>
+<%--<h1>Hello Spring MVC</h1>--%>
+<h1>${hello}</h1>
 </body>
 </html>
