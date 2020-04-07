c12e8a9d129d622e18331c77831c57a9974c4941
Le Dinh Quoc <kudo2412@gmail.com>
2020-04-07 15:27:05 +0700

    addfile

--- Java_Web/X5_Spring_Controller/Thuc_hanh/Ung_dung_login/src/controllers/UserController.java
+++ Java_Web/X5_Spring_Controller/Thuc_hanh/Ung_dung_login/src/controllers/UserController.java
@@ -1,7 +1,9 @@
 package controllers;
 
 
+import dao.UserDao;
 import model.Login;
+import model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,5 +20,17 @@ public class UserController {
     public ModelAndView home(){
         ModelAndView modelAndView = new ModelAndView("home", "login", new Login());
         return modelAndView;
+    }
+    @PostMapping("/login")
+    public ModelAndView login(@ModelAttribute("login") Login login){
+        User user = UserDao.checkLogin(login);
+        if(user == null){
+            ModelAndView modelAndView = new ModelAndView("error");
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("user");
+            modelAndView.addObject("user", user);
+            return modelAndView;
+        }
     }
 }
