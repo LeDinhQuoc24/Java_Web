d3b75c5c29eae49e914a66475689afae4d65e54e
LeDinhQuoc24 <kudo2412@gmail.com>
2020-04-13 12:15:25 +0700

    addfile

--- Java_Web/X7_Thao_tac_voi_CSDL_ORM/Bai_tap/Tao_ung_dung_Blog2/src/main/java/vnnhnlm/controller/BlogController.java
+++ Java_Web/X7_Thao_tac_voi_CSDL_ORM/Bai_tap/Tao_ung_dung_Blog2/src/main/java/vnnhnlm/controller/BlogController.java
@@ -7,11 +7,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import vnnhnlm.model.Blog;
 import vnnhnlm.service.BlogService;
 
+import java.util.List;
 
 
 @Controller
@@ -33,5 +35,53 @@ public class BlogController {
         modelAndView.addObject("blog", new Blog());
         modelAndView.addObject("message", "New blog created successfully");
         return modelAndView;
+    }
+    @GetMapping("/blogs")
+    public ModelAndView listBlogs(){
+        List<Blog> blogs = blogService.findAll();
+        ModelAndView modelAndView = new ModelAndView("/list");
+        modelAndView.addObject("blogs", blogs);
+        return modelAndView;
+    }
+    @GetMapping("/edit-blog/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id){
+        Blog blog = blogService.findById(id);
+        if(blog != null) {
+            ModelAndView modelAndView = new ModelAndView("/edit");
+            modelAndView.addObject("blog", blog);
+            return modelAndView;
+
+        }else {
+            ModelAndView modelAndView = new ModelAndView("/error.404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/edit-blog")
+    public ModelAndView updateBlog(@ModelAttribute("blog") Blog blog){
+        blogService.save(blog);
+        ModelAndView modelAndView = new ModelAndView("/edit");
+        modelAndView.addObject("blog", blog);
+        modelAndView.addObject("message", "Blog updated successfully");
+        return modelAndView;
+    }
+    @GetMapping("/delete-blog/{id}")
+    public ModelAndView showDeleteForm(@PathVariable Long id){
+        Blog blog = blogService.findById(id);
+        if(blog != null) {
+            ModelAndView modelAndView = new ModelAndView("/delete");
+            modelAndView.addObject("blog", blog);
+            return modelAndView;
+
+        }else {
+            ModelAndView modelAndView = new ModelAndView("/error.404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/delete-blog")
+    public String deleteBlog(@ModelAttribute("blog") Blog blog){
+        blogService.remove(blog.getId());
+        return "redirect:blogs";
     }
 }
