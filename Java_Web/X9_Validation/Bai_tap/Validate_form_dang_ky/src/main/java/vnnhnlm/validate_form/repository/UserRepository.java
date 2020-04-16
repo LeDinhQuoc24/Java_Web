507800a2795f31fbbb7a154529a99e4e25bacf46
LeDinhQuoc24 <kudo2412@gmail.com>
2020-04-16 20:38:00 +0700

    addfile

--- Java_Web/X9_Validation/Bai_tap/Validate_form_dang_ky/src/main/java/vnnhnlm/validate_form/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package vnnhnlm.validate_form.repository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-        import vnnhnlm.validate_form.model.User;
-
-public interface UserRepository extends PagingAndSorting<User,String>{
-}
