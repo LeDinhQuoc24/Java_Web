bcfd42292c66e43836b9ec794754ac386361a4ff
LeDinhQuoc24 <kudo2412@gmail.com>
2020-04-28 22:01:06 +0700

    addfile

--- Java_Web/Z-CaseStudy/src/main/java/casestudy/javaweb/controller/MenuController.java
+++ Java_Web/Z-CaseStudy/src/main/java/casestudy/javaweb/controller/MenuController.java
@@ -1,12 +1,23 @@
 package casestudy.javaweb.controller;
 
+import casestudy.javaweb.persistence.entity.ListLike;
 import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+@SessionAttributes("listLike")
 @Controller
 public class MenuController {
+
+    @ModelAttribute("listLike")
+    public ListLike setUpForm() {
+        return new ListLike();
+    }
 
     @GetMapping("menu")
     public ModelAndView menu() {
@@ -17,10 +28,25 @@ public class MenuController {
     public ModelAndView search() {
         return new ModelAndView("show/search");
     }
+
     @GetMapping("image")
     public ModelAndView image() {
         return new ModelAndView("show/image");
     }
 
+    @GetMapping("listLike")
+    public ModelAndView listLike(@ModelAttribute("listLike") ListLike listLike) {
+        return new ModelAndView("listLike", "listLike", listLike);
+    }
+
+    @GetMapping("listLike-add/{id}")
+    public ModelAndView addListLike(@PathVariable("id")Long id) {
+        return new ModelAndView();
+    }
+
+    @GetMapping("listLike-remove/{id}")
+    public ModelAndView removeListLike(@PathVariable("id")Long id) {
+        return new ModelAndView();
+    }
 
 }
